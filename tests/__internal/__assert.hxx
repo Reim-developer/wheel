ff6dbcdd2caa08823c4a3d762db5ec402c97476d
Reim-developer <contact.kaxtr@gmail.com>
2026-01-24 14:42:18 +0700

    Add new internal helper macro for testing

---
+++ tests/__internal/__assert.hxx
@@ -0,0 +1,27 @@
+#ifndef __ASSERT_HXX
+#define __ASSERT_HXX
+
+#include <iostream>
+#include <cstdlib>
+
+using std::cerr;
+using std::abort;
+
+#if defined(_MSC_VER)
+    #pragma warning(disable: 4806)
+#endif 
+
+#define assert(condition, message) \
+    do { \
+        if (!(condition)) { \
+            cerr << "\n"; \
+            cerr << "Assertion FAILED\n"; \
+            cerr << "Condition: " << #condition << "\n"; \
+            cerr << "Message: " << message << "\n"; \
+            cerr << "File: " << __FILE__ << "\n"; \
+            cerr << "Line: " << __LINE__ << "\n"; \
+            abort(); \
+        } \
+    } while(0)
+
+#endif // __ASSERT_HXX
