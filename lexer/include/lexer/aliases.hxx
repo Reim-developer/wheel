d9fbee943b7bfdf6f59b84534d865d64dee32765
Reim-developer <contact.kaxtr@gmail.com>
2026-01-28 20:38:45 +0700

    feat: add lexer aliases header

---
+++ lexer/include/lexer/aliases.hxx
@@ -0,0 +1,10 @@
+#if !defined(ALIASES_HXX)
+#define ALIASES_HXX
+
+#include <string_view>
+#include <cstddef>
+
+using StringView = std::string_view;
+using SizeT      = std::size_t;
+
+#endif // ALIASES_HXX
