466a6f2e955fd5b86ec3f70cbc05ca06d4381ace
Reim-developer <contact.kaxtr@gmail.com>
2026-01-29 13:50:21 +0700

    test(lexer): refactor eof test with new helper utilities

--- tests/lexer_tests/lexer_eof_test.cxx
+++ tests/lexer_tests/lexer_eof_test.cxx
@@ -1,3 +1,4 @@
+#include "__lexer_test_helper/__helper.hxx"
 #include <lexer/lexer.hxx>
 #include <lexer/kind.hxx>
 #include <__internal/__assert.hxx>
@@ -9,46 +10,15 @@ using lexer::to_string;
 using std::format;
 
 void test_eof_success_with_empty_input() {
-    DEBUG_OUTPUT(format("'test_eof_succes_with_empty_input'"));
+    DEBUG_OUTPUT(format("'test_eof_success_with_empty_input'"));
     const char* source = "";
 
     Lexer lexer(source);
-    auto token          = lexer.next_token();
-    auto token_type     = to_string(token.kind);
-    auto line_no        = token.source_location.line;
-    auto line_col       = token.source_location.column;
-    auto line_offset    = token.source_location.offset;
+    auto token = lexer.next_token();
 
-    internal_assert(token.kind == TokenKind::EOF_, format(
-        "Expected {}, but got {} of <token.kind>",
-        to_string(TokenKind::EOF_), token_type
-    ));
-
-    internal_assert(line_no == 1, format(
-        "Expected {}, but got {} of <token.source_location.line>",
-        1, line_no
-    ));
-     internal_assert(line_col == 1, format(
-        "Expected {}, but got {} of <token.source_location.column>",
-        1, line_col
-    ));
-     internal_assert(line_offset == 0, format(
-        "Expected {}, but got {} of <token.source_location.offset>",
-        1, line_offset
-    ));
-
-    DEBUG_OUTPUT(format(
-        "<token_type>: {}", token_type
-    ));
-    DEBUG_OUTPUT(format(
-        "<line_no>: {}", line_no
-    ));
-    DEBUG_OUTPUT(format(
-        "<line_col>: {}", line_col 
-    ));
-      DEBUG_OUTPUT(format(
-        "<line_offset>: {}", line_offset
-    ));
+    Verify<lexer::TokenKind::EOF_>{}.token_as(lexer::TokenKind::EOF_);
+    Verify<lexer::TokenKind::EOF_>{}.token_str_eq_to(token, "", 0);
+    Verify<lexer::TokenKind::EOF_>{}.token_metadata_eq_to(token, 1, 1, 0);
 }
 
 int main() {
