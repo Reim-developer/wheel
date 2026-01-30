2af427fa7e5512b2b78c8ddc70313cce58b93b41
Reim-developer <contact.kaxtr@gmail.com>
2026-01-30 20:11:23 +0700

    feat: Add SPACE, COMMENT, and DOCUMENT_COMMENT token kinds with performance optimizations

--- lexer/include/lexer/kind.hxx
+++ lexer/include/lexer/kind.hxx
@@ -2,6 +2,7 @@
 #define KIND_HXX
 
 #include <cstdint>
+#include "aliases.hxx"
 
 namespace lexer {
     enum class TokenKind : uint8_t {
@@ -10,6 +11,16 @@ namespace lexer {
 
         TAB,
         NEWLINE,
+        SPACE,
+
+        /*
+        C-style comments.
+        */
+        COMMENT,
+        /*
+        Document comment.
+        */
+        DOCUMENT_COMMENT,
 
         IDENT,
         
@@ -47,29 +58,32 @@ namespace lexer {
         RIGHT_PARENT
     };
 
-    [[nodiscard]] const char* to_string(TokenKind token_kind) noexcept {
+    WHEEL_COMPTIME_NODISCARD_INLINE const char* to_string(TokenKind token_kind) noexcept {
         switch (token_kind) {
-            case TokenKind::EOF_:           return "EOF";
-            case TokenKind::ERROR:          return "ERROR";
-            case TokenKind::TAB:            return "TAB";
-            case TokenKind::NEWLINE:        return "NEWLINE";
-            case TokenKind::IDENT:          return "IDENT";
-            case TokenKind::INT_LITERAL:    return "INT_LITERAL";
-            case TokenKind::FLOAT_LITERAL:  return "FLOAT_LITERAL";
-            case TokenKind::STRING_LITERAL: return "STRING_LITERAL";
-            case TokenKind::EQUAL:          return "EQUAL";
-            case TokenKind::PLUS:           return "PLUS";
-            case TokenKind::MINUS:          return "MINUS";
-            case TokenKind::STAR:           return "STAR";
-            case TokenKind::SLASH:          return "SLASH";
-            case TokenKind::EQUAL_EQUAL:    return "EQUAL_EQUAL";
-            case TokenKind::NOT_EQUAL:      return "NOT_EQUAL";
-            case TokenKind::LESS_THAN:      return "LESS_THAN";
-            case TokenKind::GREATER_THAN:   return "GREATER_THAN";
-            case TokenKind::COLON:          return "COLON";
-            case TokenKind::COMMA:          return "COMMA";
-            case TokenKind::LEFT_PAREN:     return "LEFT_PARENT";
-            case TokenKind::RIGHT_PARENT:   return  "RIGHT_PARENT";
+            case TokenKind::EOF_:               return "EOF";
+            case TokenKind::ERROR:              return "ERROR";
+            case TokenKind::TAB:                return "TAB";
+            case TokenKind::SPACE:              return "SPACE";
+            case TokenKind::COMMENT:            return "COMMENT";
+            case TokenKind::DOCUMENT_COMMENT:   return "DOCUMENT_COMMENT";
+            case TokenKind::NEWLINE:            return "NEWLINE";
+            case TokenKind::IDENT:              return "IDENT";
+            case TokenKind::INT_LITERAL:        return "INT_LITERAL";
+            case TokenKind::FLOAT_LITERAL:      return "FLOAT_LITERAL";
+            case TokenKind::STRING_LITERAL:     return "STRING_LITERAL";
+            case TokenKind::EQUAL:              return "EQUAL";
+            case TokenKind::PLUS:               return "PLUS";
+            case TokenKind::MINUS:              return "MINUS";
+            case TokenKind::STAR:               return "STAR";
+            case TokenKind::SLASH:              return "SLASH";
+            case TokenKind::EQUAL_EQUAL:        return "EQUAL_EQUAL";
+            case TokenKind::NOT_EQUAL:          return "NOT_EQUAL";
+            case TokenKind::LESS_THAN:          return "LESS_THAN";
+            case TokenKind::GREATER_THAN:       return "GREATER_THAN";
+            case TokenKind::COLON:              return "COLON";
+            case TokenKind::COMMA:              return "COMMA";
+            case TokenKind::LEFT_PAREN:         return "LEFT_PARENT";
+            case TokenKind::RIGHT_PARENT:       return  "RIGHT_PARENT";
         }
 
         return "ERROR";
