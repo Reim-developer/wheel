3bca102b1077990b2e946eafbef34375b2578be7
Reim-developer <contact.kaxtr@gmail.com>
2026-01-30 20:11:15 +0700

    feat: Add space and slash token handlers with comment parsing support

--- lexer/include/lexer/handlers.hxx
+++ lexer/include/lexer/handlers.hxx
@@ -1,8 +1,10 @@
 #if !defined(HANDLERS_HXX)
-#define HANDLERs_HXX
+#define HANDLERS_HXX
 #include "cursor.hxx"
 #include "token.hxx"
 #include "utils.hxx"
+#include "aliases.hxx"
+#include "properties.hxx"
 #include <string_view>
 #include <array>
 
@@ -10,6 +12,8 @@ namespace lexer {
     using TokenHandler = Token(*)(Cursor&, size_t);
     using StrView      = std::string_view;
     using Kind         = TokenKind;
+
+    #define _SOURCE_TEXT(cursor, start) cursor.source_view.substr(start, cursor.position() - start) 
 
     [[nodiscard]] Token if_tab(Cursor& cursor, size_t start) noexcept {
         cursor.bump();
@@ -63,6 +67,61 @@ namespace lexer {
         return make_token(Kind::INT_LITERAL, source_text, start, cursor.position());
     }
 
+    WHEEL_ALWAYS_INLINE_NODISCARD Token if_space(Cursor &cursor, size_t start) noexcept {
+        cursor.bump();
+
+        StrView source_text = _SOURCE_TEXT(cursor, start);
+        return make_token(Kind::SPACE, source_text, start, cursor.position());
+    }
+
+    WHEEL_ALWAYS_INLINE_NODISCARD Token if_slash(Cursor &cursor, size_t start) noexcept {
+        cursor.bump();
+
+        const char second = cursor.first();
+        if(second != '/' && second != '*') {
+            
+            return make_token(Kind::SLASH, _SOURCE_TEXT(cursor, start), start, cursor.position());
+        }
+
+        switch(second) {
+            case '/': {
+                const char third  = cursor.second();
+                bool       is_doc = (third == '/');
+
+                Kind       kind   = is_doc ? Kind::DOCUMENT_COMMENT : Kind::COMMENT;
+
+                cursor.bump();
+                if (is_doc) cursor.bump();
+
+                while(!cursor.is_eof() && !properties::is_newline_like(cursor.first())) {
+                    cursor.bump();
+                }
+
+                return make_token(kind, _SOURCE_TEXT(cursor, start), start, cursor.position());
+            }
+
+            case '*': {
+                Kind kind = Kind::COMMENT;
+                cursor.bump();
+
+                while (!cursor.is_eof()) {
+                    if (cursor.first() == '*' && cursor.second() == '/') {
+                        cursor.bump();
+                        cursor.bump();
+
+                        break;
+                    }
+
+                    cursor.bump();
+                }
+
+                return make_token(kind, _SOURCE_TEXT(cursor, start), start, cursor.position());
+            }
+        }
+
+        return make_token(Kind::ERROR, _SOURCE_TEXT(cursor, start), start, cursor.position());
+    }
+ 
     constexpr std::array<TokenHandler, 256> HANDLERS = []() {
         std::array<TokenHandler, 256> table = {};
 
@@ -70,6 +129,8 @@ namespace lexer {
         table['\t'] = if_tab;
         table['\r'] = if_newline;
         table['\n'] = if_newline;
+        table[' ']  = if_space;
+        table['/']  = if_slash;
 
         /* Operator(s) */
         table['=']  = if_equal;
