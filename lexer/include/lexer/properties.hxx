eb81ffc487354d583229d1be2f3efffac78b4139
Reim-developer <contact.kaxtr@gmail.com>
2026-01-30 20:11:42 +0700

    refactor: Move character properties to lexer::properties namespace and add performance optimizations

--- lexer/include/lexer/properties.hxx
+++ lexer/include/lexer/properties.hxx
@@ -1,15 +1,16 @@
 #if !defined(PROPERTIES_HXX)
 #define PROPERTIES_HXX
+#include "aliases.hxx"
 
 #include <cstdint>
 #include <array>
 
-namespace lexer {
+namespace lexer::properties {
     enum CharFlags : uint8_t {
-        NONE       = 0,
-        NEWLINE    = 1 << 0,
-        IS_CR      = 1 << 1,
-        PREV_WAS_CR  = 1 << 2
+        NONE            = 0,
+        NEWLINE         = 1 << 0,
+        IS_CR           = 1 << 1,
+        PREV_WAS_CR     = 1 << 2
     };
 
     constexpr std::array<uint8_t, 256> CHAR_PROPERTIES = []() {
@@ -21,11 +22,11 @@ namespace lexer {
         return properties;
     }();
 
-    [[nodiscard]] constexpr bool is_newline_like(char character) noexcept {
+    WHEEL_COMPTIME_NODISCARD_INLINE bool is_newline_like(char character) noexcept {
         return CHAR_PROPERTIES[static_cast<unsigned char>(character)] & (NEWLINE);
     }
 
-    [[nodiscard]] constexpr bool is_crlf_sequence(char current_char, char prev_char) noexcept {
+    WHEEL_COMPTIME_NODISCARD_INLINE bool is_crlf_sequence(char current_char, char prev_char) noexcept {
         return 
             (CHAR_PROPERTIES[static_cast<unsigned char>(current_char)] & PREV_WAS_CR) &&
             (CHAR_PROPERTIES[static_cast<unsigned char>(prev_char)] & IS_CR);
