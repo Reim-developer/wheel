eb6a008c10dffd072b0c4855985d8cbc24a01dff
Reim-developer <contact.kaxtr@gmail.com>
2026-01-31 15:20:56 +0700

    test: Add comprehensive space token handling tests to validate lexer behavior with various whitespace scenarios

---
+++ tests/lexer_tests/lexer_space_test.cxx
@@ -0,0 +1,62 @@
+#include "__lexer_test_helper/__helper.hxx"
+#include <lexer/lexer.hxx>
+
+using lexer::Lexer;
+using lexer::TokenKind;
+
+TEST(test_normal_space)
+    const char* source = " ";
+
+    Lexer lexer(source);
+    auto token = lexer.next_token();
+
+    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(token, 1, 2, 0);
+    VERIFY_THIS(TokenKind::SPACE).token_as(token.kind);
+    VERIFY_THIS(TokenKind::SPACE).token_str_eq_to(token, " ", STR_LEN(source));
+DONE
+
+TEST(test_normal_space_with_variable_declaration)
+    const char* source = "int x = 40";
+
+    Lexer lexer(source);
+    auto t1 = lexer.next_token();
+    auto t2 = lexer.next_token();
+    auto t3 = lexer.next_token();
+    auto t4 = lexer.next_token();
+    auto t5 = lexer.next_token();
+    auto t6 = lexer.next_token();
+    auto t7 = lexer.next_token();
+
+    VERIFY_THIS(TokenKind::IDENT).token_str_eq_to(t1, "int", 3);
+    VERIFY_THIS(TokenKind::IDENT).token_as(t1.kind);
+    VERIFY_THIS(TokenKind::IDENT).token_metadata_eq_to(t1, 1, 4, 0);
+
+    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(t2, 1, 5, 3);
+    VERIFY_THIS(TokenKind::SPACE).token_as(t2.kind);
+    VERIFY_THIS(TokenKind::SPACE).token_str_eq_to(t2, " ", 1);
+
+    VERIFY_THIS(TokenKind::IDENT).token_metadata_eq_to(t3, 1, 6, 4);
+    VERIFY_THIS(TokenKind::IDENT).token_as(t3.kind);
+    VERIFY_THIS(TokenKind::IDENT).token_str_eq_to(t3, "x", 1);
+
+    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(t4, 1, 7, 5);
+    VERIFY_THIS(TokenKind::SPACE).token_as(t4.kind);
+    VERIFY_THIS(TokenKind::SPACE).token_str_eq_to(t4, " ", 1);
+
+    VERIFY_THIS(TokenKind::EQUAL).token_metadata_eq_to(t5, 1, 8, 6);
+    VERIFY_THIS(TokenKind::EQUAL).token_as(t5.kind);
+    VERIFY_THIS(TokenKind::EQUAL).token_str_eq_to(t5, "=", 1);
+
+    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(t6, 1, 9, 7);
+    VERIFY_THIS(TokenKind::SPACE).token_as(t6.kind);
+    VERIFY_THIS(TokenKind::SPACE).token_str_eq_to(t6, " ", 1);
+
+    VERIFY_THIS(TokenKind::INT_LITERAL).token_metadata_eq_to(t7, 1, 11, 8);
+    VERIFY_THIS(TokenKind::INT_LITERAL).token_as(t7.kind);
+    VERIFY_THIS(TokenKind::INT_LITERAL).token_str_eq_to(t7, "40", 2);
+DONE
+
+TEST_ENTRY
+    RUN_TEST(test_normal_space)
+    RUN_TEST(test_normal_space_with_variable_declaration)
+ENTRY_END
