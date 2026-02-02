1ddb2ac0b27f297faef5fb61bc4b251db6fd9c17
Reim-developer <contact.kaxtr@gmail.com>
2026-02-02 13:43:57 +0700

    Update lexer EOF test to use correct include paths

--- tests/lexer_tests/lexer_eof_test.cxx
+++ tests/lexer_tests/lexer_eof_test.cxx
@@ -1,12 +1,12 @@
 #include "__lexer_test_helper/__helper.hxx"
-#include <lexer/lexer.hxx>
-#include <lexer/kind.hxx>
+#include <lib/lexer.hxx>
+#include <lib/kind.hxx>
 #include <__internal/__assert.hxx>
 #include <format>
 
-using lexer::Lexer;
-using lexer::TokenKind;
-using lexer::to_string;
+using wheel_lexer::Lexer;
+using wheel_lexer::TokenKind;
+using wheel_lexer::to_string;
 using std::format;
 
 TEST(test_eof_success_with_empty_input)
@@ -15,10 +15,9 @@ TEST(test_eof_success_with_empty_input)
     Lexer lexer(source);
     auto token = lexer.next_token();
 
-    Verify<lexer::TokenKind::EOF_>{}.token_as(lexer::TokenKind::EOF_);
-    Verify<lexer::TokenKind::EOF_>{}.token_str_eq_to(token, "", 0);
-    Verify<lexer::TokenKind::EOF_>{}.token_metadata_eq_to(token, 1, 1, 0);
-
+    VERIFY_THIS(TokenKind::EOF_).token_as(TokenKind::EOF_);
+    VERIFY_THIS(TokenKind::EOF_).token_str_eq_to(token, "", 0);
+    VERIFY_THIS(TokenKind::EOF_).token_metadata_eq_to(token, 1, 1, 0);
 DONE
 
 TEST_ENTRY
