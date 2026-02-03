913bf78e4241c1606108761a2abfc79599ba8119
Reim-developer <contact.kaxtr@gmail.com>
2026-02-03 16:55:05 +0700

    Add random string utility function and required includes for test assertions

--- tests/__internal/__assert.hxx
+++ tests/__internal/__assert.hxx
@@ -4,11 +4,17 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <string>
+#include <random>
 
 using std::cerr;
 using std::cout;
 using std::boolalpha;
 using std::abort;
+using std::string;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
 
 #if defined(_MSC_VER)
     #pragma warning(disable: 4806)
@@ -42,5 +48,26 @@ using std::abort;
             abort(); \
         } \
     } while(0)
+
+string random_str(size_t length) {
+    const char char_set[] = 
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "\n\t";
+    
+    random_device random;
+    mt19937       genrator(random());
+    uniform_int_distribution<size_t> distribution(0, sizeof(char_set) - 2);
+
+    string result;
+    result.reserve(length);
+
+    for (size_t index = 0; index < length; ++index) {
+        result += char_set[distribution(genrator)];
+    }
+
+    return result;
+}
 
 #endif // __ASSERT_HXX
