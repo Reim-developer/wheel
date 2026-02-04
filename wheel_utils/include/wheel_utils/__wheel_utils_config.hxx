7c27a65d6604b62121d7fb1539478190d2f5f4ca
Reim-developer <contact.kaxtr@gmail.com>
2026-02-04 15:25:58 +0700

    feat: add wheel_utils module with logging functionality

---
+++ wheel_utils/include/wheel_utils/__wheel_utils_config.hxx
@@ -0,0 +1,7 @@
+#if !defined(__WHEEL_UTILS_CONFIG_HXX)
+#define  __WHEEL_UTILS_CONFIG_HXX
+
+#define WHEEL_UTILS_NAMESPACE namespace wheel_utils {
+#define WHEEL_UTILS_END_NAMESPACE }
+
+#endif // __WHEEL_UTILS_CONFIG_HXX
---
+++ wheel_utils/include/wheel_utils/logging.hxx
@@ -0,0 +1,44 @@
+#if !defined(LOGGING_HXX)
+#define LOGGING_HXX
+#include "__wheel_utils_config.hxx"
+
+#include <iostream>
+#include <format>
+#include <cstring>
+
+WHEEL_UTILS_NAMESPACE 
+
+#if defined(WHEEL_DEBUG)
+    #if defined(_MSVC_LANG)
+        #define CURRENT_CPP_VERSION _MSVC_LANG
+
+    #else 
+        #define CURRENT_CPP_VERSION __cplusplus
+    #endif 
+
+    #if CURRENT_CPP_VERSION >= 202002L
+        #if defined(_WIN32)
+            #define WHEEL_BASE_NAME(path) ((std::strrchr(path, '\\')) ? std::strrchr(path, '\\') + 1 : path)
+
+        #else
+            #define WHEEL_BASE_NAME(path) ((std::strrchr(path, '/')) ? std::strrchr(path, '/') + 1 : path)
+
+        #endif 
+
+        #define DEBUG_PRINT(msg) \
+            do { \
+                const auto fmt = std::format("[DEBUG] [{}:{}] {}", __LINE__, WHEEL_BASE_NAME(__FILE__), msg);\
+                std::cout << fmt << "\n";\
+            } while(0)
+    
+    #else
+        #define DEBUG_PRINT(msg) ((void)0)
+    #endif
+
+#else 
+    #define DEBUG_PRINT(msg) ((void)0)
+#endif 
+
+WHEEL_UTILS_END_NAMESPACE
+
+#endif // LOGGING_HXX
