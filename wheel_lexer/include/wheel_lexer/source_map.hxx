c2f816f20db50d74a72e3af4e1610cb4406f2ab7
Reim-developer <contact.kaxtr@gmail.com>
2026-02-06 00:15:15 +0700

    Add source_map.hxx: Implement source location mapping for accurate token position tracking

---
+++ wheel_lexer/include/wheel_lexer/source_map.hxx
@@ -0,0 +1,63 @@
+#if !defined(SOURCE_MAP_HXX)
+#define SOURCE_MAP_HXX
+
+#include <vector>
+#include <algorithm>
+#include "aliases.hxx"
+#include "token.hxx"
+#include "properties.hxx"
+
+WHEEL_LEXER_NAMESPACE
+    struct SourceMap {
+        private:
+           std::vector<uint32_t> line_starts;
+
+        public:
+            SourceMap () {
+                line_starts.push_back(0);
+            }
+           
+            WHEEL_ALWAYS_INLINE void build_table(StringView source) noexcept {
+                line_starts.clear();
+                line_starts.push_back(0);
+
+                for (size_t index = 0; index < source.size(); ++index) {
+                    char character = source[index];
+
+                    if(is_crlf_sequence(character, (index > 0) ? source[index -1] : '\0' )) {
+                        continue;
+                    }
+
+                    if(is_newline_like(character)) {
+                        line_starts.push_back(static_cast<uint32_t>(index + 1));
+                    }
+                }
+            }
+
+            WHEEL_ALWAYS_INLINE_NODISCARD SourceLocation source_location(size_t offset) const {
+                if(offset > UINT32_MAX) {
+                    return SourceLocation {0, 0, offset};
+                }
+
+                uint32_t safe_offset = static_cast<uint32_t>(offset);
+                auto it              = std::upper_bound(line_starts.begin(), line_starts.end(), safe_offset);
+
+                if (it != line_starts.begin()) {
+                    --it;
+                } 
+
+                size_t line_index           = std::distance(line_starts.begin(), it);
+                uint32_t line_start_offset  = line_starts[line_index];
+                uint32_t column             = (safe_offset - line_start_offset) + 1;
+
+                return SourceLocation {
+                    .line   = line_index + 1,
+                    .column = column,
+                    .offset = safe_offset
+                };
+            }
+    };
+
+END_NAMESPACE
+
+#endif // SOURCE_MAP_HXX
