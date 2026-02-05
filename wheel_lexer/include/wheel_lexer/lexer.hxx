c8425c64000671bb5ba6c88c3fe7bccdcb792917
Reim-developer <contact.kaxtr@gmail.com>
2026-02-06 00:14:42 +0700

    Update lexer.hxx: Include source_map.hxx and enhance location tracking with CRLF sequence handling

--- wheel_lexer/include/wheel_lexer/lexer.hxx
+++ wheel_lexer/include/wheel_lexer/lexer.hxx
@@ -7,6 +7,7 @@
 #include "handlers.hxx"
 #include "aliases.hxx"
 #include "properties.hxx"
+#include "source_map.hxx"
 #include "__wheel_lexer_config.hxx"
 
 WHEEL_LEXER_NAMESPACE
@@ -15,10 +16,11 @@ WHEEL_LEXER_NAMESPACE
 
         private:
             Cursor          cursor;
+            SourceMap       source_map;
             SourceLocation  source_location;
 
             void update_location(char character) {
-                if(is_newline_like(character)) {
+                if (is_newline_like(character)) {
                     source_location.line++;
                     source_location.column = 1;
                 } else {
@@ -27,9 +29,9 @@ WHEEL_LEXER_NAMESPACE
             }
 
         public:
-            explicit Lexer(StringView source_text) : 
-                cursor(source_text), 
-                source_location{1, 1, 0} {}
+            explicit Lexer(StringView source_text) : cursor(source_text), source_location {1, 1, 0} {
+                source_map.build_table(source_text);
+            }
 
             WHEEL_ALWAYS_INLINE_NODISCARD Token next_token() {
                 if(cursor.is_eof()) {
@@ -42,31 +44,27 @@ WHEEL_LEXER_NAMESPACE
                 if (auto handler = get_handler(character)) {
                     Token token = handler(cursor, start);
 
-                    for(size_t index = 0; index < token.str.size(); ++index) {
+                    for (size_t index = 0; index < token.str.size(); ++index) {
                         char character = token.str[index];
-
                         char prev_char = (index > 0) ? token.str[index - 1] : '\0';
+
                         if(is_crlf_sequence(character, prev_char)) {
                             source_location.column = 1;
                         } else {
                             update_location(character);
                         }
                     }
- 
-                    source_location.offset = start;
 
-                    return Token {
-                        token.kind, token.str,
-                        token.start, token.end, source_location
-                    };
+                    return token;
                 }
 
-                update_location(character);
                 cursor.bump();
-                source_location.offset = start;
-
                 return make_error(start, StringView(&character, 1));
             }
+
+        WHEEL_ALWAYS_INLINE_NODISCARD SourceLocation get_source_location(const Token &token) noexcept {
+            return source_map.source_location(token.start);
+        }
     };
 
 END_NAMESPACE
