14800fb888a722f5d600547c24954f69b8da793d
Reim-developer <contact.kaxtr@gmail.com>
2026-02-06 00:12:17 +0700

    Update lexer_space_test.cxx: Add variable declaration test with space token verification

--- tests/lexer_tests/lexer_space_test.cxx
+++ tests/lexer_tests/lexer_space_test.cxx
@@ -10,7 +10,7 @@ TEST(test_normal_space)
     Lexer lexer(source);
     auto token = lexer.next_token();
 
-    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(token, 1, 2, 0);
+    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(lexer.get_source_location(token), 1, 1, 0);
     VERIFY_THIS(TokenKind::SPACE).token_as(token.kind);
     VERIFY_THIS(TokenKind::SPACE).token_str_eq_to(token, " ", STR_LEN(source));
 DONE
@@ -29,29 +29,29 @@ TEST(test_normal_space_with_variable_declaration)
 
     VERIFY_THIS(TokenKind::IDENT).token_str_eq_to(t1, "int", 3);
     VERIFY_THIS(TokenKind::IDENT).token_as(t1.kind);
-    VERIFY_THIS(TokenKind::IDENT).token_metadata_eq_to(t1, 1, 4, 0);
+    VERIFY_THIS(TokenKind::IDENT).token_metadata_eq_to(lexer.get_source_location(t1), 1, 1, 0);
 
-    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(t2, 1, 5, 3);
+    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(lexer.get_source_location(t2), 1, 4, 3);
     VERIFY_THIS(TokenKind::SPACE).token_as(t2.kind);
     VERIFY_THIS(TokenKind::SPACE).token_str_eq_to(t2, " ", 1);
 
-    VERIFY_THIS(TokenKind::IDENT).token_metadata_eq_to(t3, 1, 6, 4);
+    VERIFY_THIS(TokenKind::IDENT).token_metadata_eq_to(lexer.get_source_location(t3), 1, 5, 4);
     VERIFY_THIS(TokenKind::IDENT).token_as(t3.kind);
     VERIFY_THIS(TokenKind::IDENT).token_str_eq_to(t3, "x", 1);
 
-    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(t4, 1, 7, 5);
+    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(lexer.get_source_location(t4), 1, 6, 5);
     VERIFY_THIS(TokenKind::SPACE).token_as(t4.kind);
     VERIFY_THIS(TokenKind::SPACE).token_str_eq_to(t4, " ", 1);
 
-    VERIFY_THIS(TokenKind::EQUAL).token_metadata_eq_to(t5, 1, 8, 6);
+    VERIFY_THIS(TokenKind::EQUAL).token_metadata_eq_to(lexer.get_source_location(t5), 1, 7, 6);
     VERIFY_THIS(TokenKind::EQUAL).token_as(t5.kind);
     VERIFY_THIS(TokenKind::EQUAL).token_str_eq_to(t5, "=", 1);
 
-    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(t6, 1, 9, 7);
+    VERIFY_THIS(TokenKind::SPACE).token_metadata_eq_to(lexer.get_source_location(t6), 1, 8, 7);
     VERIFY_THIS(TokenKind::SPACE).token_as(t6.kind);
     VERIFY_THIS(TokenKind::SPACE).token_str_eq_to(t6, " ", 1);
 
-    VERIFY_THIS(TokenKind::INT_LITERAL).token_metadata_eq_to(t7, 1, 11, 8);
+    VERIFY_THIS(TokenKind::INT_LITERAL).token_metadata_eq_to(lexer.get_source_location(t7), 1, 9, 8);
     VERIFY_THIS(TokenKind::INT_LITERAL).token_as(t7.kind);
     VERIFY_THIS(TokenKind::INT_LITERAL).token_str_eq_to(t7, "40", 2);
 DONE
