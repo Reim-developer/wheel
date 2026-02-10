e416563afb767deff4a7fa049acc5f520671fce1
Reim-developer <contact.kaxtr@gmail.com>
2026-02-10 16:19:42 +0700

    feat: add parser interface header for recursive descent implementation

---
+++ wheel_parser/include/wheel_parser/parser.hxx
@@ -0,0 +1,16 @@
+#if !defined(PARSER_HXX)
+#define PARSER_HXX
+#include "config.hxx"
+#include <wheel_lexer/lexer.hxx>
+#include <wheel_lexer/token.hxx>
+
+using namespace wheel_lexer;
+
+WHEEL_PARSER_NAMESPACE
+    class WheelParser {
+        public:
+            WheelParser();
+    };
+
+WHEEL_PARSER_END_NAMESPACE
+#endif // PARSER_HXX
