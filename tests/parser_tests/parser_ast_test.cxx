4d7a63fa2539cb9c9fb7cbcbfca263aac4823335
Reim-developer <contact.kaxtr@gmail.com>
2026-02-13 22:58:00 +0700

    test: add tests for AST block statement creation and nested blocks

--- tests/parser_tests/parser_ast_test.cxx
+++ tests/parser_tests/parser_ast_test.cxx
@@ -43,6 +43,60 @@ TEST(test_ast_function_declaration_creation)
     assert_eq(func->token, nullptr);
 DONE 
 
+TEST(test_ast_block_statement_creation)
+    Arena arena;
+
+    BlockStatement *stmt1 = arena.allocate<BlockStatement>(
+        nullptr, std::vector<StatementNode*>{}
+    );
+    BlockStatement *stmt2 = arena.allocate<BlockStatement>(
+        nullptr, std::vector<StatementNode*>{}
+    );
+    BlockStatement *block_stmt = arena.allocate<BlockStatement>(
+        nullptr, std::vector<StatementNode*>{stmt1, stmt2}
+    );
+    
+    assert_eq(block_stmt->node_kind, NodeKind::BlockStatement);
+    assert_eq(block_stmt->statements.size(), 2);
+    assert_eq(block_stmt->statements[0], stmt1);
+    assert_eq(block_stmt->statements[1], stmt2);
+DONE 
+
+TEST(test_ast_nested_blocks)
+    Arena arena;
+    StringInterner interner;
+
+    auto func_name = interner.intern("nested_func");
+    auto param     = interner.intern("param");
+
+    BlockStatement *block1 = arena.allocate<BlockStatement>(
+        nullptr,
+        std::vector<StatementNode*>{}
+    );
+    BlockStatement *block2 = arena.allocate<BlockStatement>(
+        nullptr,
+        std::vector<StatementNode*>{block1}
+    );
+
+    FunctionDeclaration *func = arena.allocate<FunctionDeclaration>(
+        nullptr, 
+        func_name,
+        std::vector<SymbolID>{param},
+        block2
+    );
+
+    assert_eq(func->func_name, func_name);
+    assert_eq(func->func_params.size(), 1);
+    assert_eq(func->func_params[0], param);
+
+    BlockStatement *func_body = static_cast<BlockStatement*>(func->func_body);
+    assert_eq(func_body->node_kind, NodeKind::BlockStatement);
+    assert_eq(func_body->statements.size(), 1);
+    assert_eq(func_body->token, nullptr);
+DONE 
+
 TEST_MAIN
     RUN(test_ast_function_declaration_creation)
+    RUN(test_ast_block_statement_creation)
+    RUN(test_ast_nested_blocks)
 END_MAIN
