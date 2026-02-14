313bc34373b126152a3e6db9ba82a36c1619eb3c
Reim-developer <contact.kaxtr@gmail.com>
2026-02-14 14:28:41 +0700

    test(parser): add variable declaration test and use TOKEN macro

--- tests/parser_tests/parser_ast_test.cxx
+++ tests/parser_tests/parser_ast_test.cxx
@@ -1,18 +1,20 @@
 #include <string>
-#include <format>
 #include <wheel_parser/ast/nodes.hxx>
 #include <wheel_parser/ast/symbol.hxx>
 #include <wheel_parser/allocator.hxx>
+#include <wheel_lexer/token.hxx>
 #include <__internal/__assert.hxx>
 
 using std::string;
 using wheel_parser::ast::StringInterner;
-using wheel_parser::ast::Node;
 using wheel_parser::ast::StatementNode;
 using wheel_parser::ast::BlockStatement;
 using wheel_parser::ast::FunctionDeclaration;
+using wheel_parser::ast::VariableDeclaration;
 using wheel_parser::ast::NodeKind;
+using wheel_parser::ast::LiteralExpression;
 using wheel_parser::ast::SymbolID;
+using wheel_lexer::TokenKind;
 using wheel_parser::Arena;
 
 TEST(test_ast_function_declaration_creation)
@@ -26,10 +28,10 @@ TEST(test_ast_function_declaration_creation)
     };
     
     BlockStatement *func_body = arena.allocate<BlockStatement>(
-        nullptr, std::vector<StatementNode*>{}
+        TOKEN(TokenKind::IDENT, arena), std::vector<StatementNode*>{}
     );
     FunctionDeclaration *func = arena.allocate<FunctionDeclaration>(
-        nullptr,
+        TOKEN(TokenKind::IDENT, arena),
         func_name,
         params,
         func_body
@@ -40,26 +42,29 @@ TEST(test_ast_function_declaration_creation)
     assert_eq(func->func_params, params);
     assert_eq(func->func_name, func_name);
     assert_eq(func->func_body, func_body);
-    assert_eq(func->token, nullptr);
+    assert_eq(func->token->kind, TokenKind::IDENT);
 DONE 
 
 TEST(test_ast_block_statement_creation)
     Arena arena;
 
     BlockStatement *stmt1 = arena.allocate<BlockStatement>(
-        nullptr, std::vector<StatementNode*>{}
+        TOKEN(TokenKind::IDENT, arena), std::vector<StatementNode*>{}
     );
     BlockStatement *stmt2 = arena.allocate<BlockStatement>(
-        nullptr, std::vector<StatementNode*>{}
+        TOKEN(TokenKind::IDENT, arena), std::vector<StatementNode*>{}
     );
     BlockStatement *block_stmt = arena.allocate<BlockStatement>(
-        nullptr, std::vector<StatementNode*>{stmt1, stmt2}
+        TOKEN(TokenKind::IDENT, arena), std::vector<StatementNode*>{stmt1, stmt2}
     );
     
     assert_eq(block_stmt->node_kind, NodeKind::BlockStatement);
     assert_eq(block_stmt->statements.size(), 2);
     assert_eq(block_stmt->statements[0], stmt1);
     assert_eq(block_stmt->statements[1], stmt2);
+    assert_eq(block_stmt->statements[0]->token->kind, TokenKind::IDENT);
+    assert_eq(block_stmt->statements[1]->token->kind, TokenKind::IDENT);
+    assert_eq(block_stmt->token->kind, TokenKind::IDENT);
 DONE 
 
 TEST(test_ast_nested_blocks)
@@ -70,16 +75,16 @@ TEST(test_ast_nested_blocks)
     auto param     = interner.intern("param");
 
     BlockStatement *block1 = arena.allocate<BlockStatement>(
-        nullptr,
+        TOKEN(TokenKind::IDENT, arena),
         std::vector<StatementNode*>{}
     );
     BlockStatement *block2 = arena.allocate<BlockStatement>(
-        nullptr,
+        TOKEN(TokenKind::IDENT, arena),
         std::vector<StatementNode*>{block1}
     );
 
     FunctionDeclaration *func = arena.allocate<FunctionDeclaration>(
-        nullptr, 
+        TOKEN(TokenKind::IDENT, arena), 
         func_name,
         std::vector<SymbolID>{param},
         block2
@@ -92,11 +97,36 @@ TEST(test_ast_nested_blocks)
     BlockStatement *func_body = static_cast<BlockStatement*>(func->func_body);
     assert_eq(func_body->node_kind, NodeKind::BlockStatement);
     assert_eq(func_body->statements.size(), 1);
-    assert_eq(func_body->token, nullptr);
+    assert_eq(func_body->token->kind, TokenKind::IDENT);
+DONE 
+
+TEST(test_ast_variable_literal_declaration_creation)
+    Arena arena;
+    StringInterner interner;
+
+    const auto var_name = interner.intern("my_variable");
+    const auto var_type = interner.intern("int");
+
+    LiteralExpression *literal = arena.allocate<LiteralExpression>(
+        TOKEN(TokenKind::INT_LITERAL, arena)
+    );
+    VariableDeclaration *variable = arena.allocate<VariableDeclaration>(
+        TOKEN(TokenKind::IDENT, arena),
+        var_type, var_name,
+        literal
+    );
+
+    assert_eq(variable->initializer->node_kind, NodeKind::LiteralExpression);
+    assert_eq(variable->var_name, var_name);
+    assert_eq(variable->var_type, var_type);
+    assert_eq(variable->token->kind, TokenKind::IDENT);
+    assert_eq(variable->initializer, literal);
+    assert_eq(variable->initializer->token->kind, TokenKind::INT_LITERAL);
 DONE 
 
 TEST_MAIN
     RUN(test_ast_function_declaration_creation)
     RUN(test_ast_block_statement_creation)
     RUN(test_ast_nested_blocks)
+    RUN(test_ast_variable_literal_declaration_creation)
 END_MAIN
