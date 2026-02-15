fc906b98c48191825edf2002db938068c3133af1
Reim-developer <contact.kaxtr@gmail.com>
2026-02-15 19:06:15 +0700

    perf(lexer): add likely/unlikely hints to string comparison macros

--- wheel_lexer/include/wheel_lexer/aliases.hxx
+++ wheel_lexer/include/wheel_lexer/aliases.hxx
@@ -4,13 +4,13 @@
 #include <string_view>
 #include <cstddef>
 
-#define WHEEL_STR_LIKELY(str1, str2)   str1 == str2
-#define WHEEL_STR_UNLIKELY(str1, str2) str1 != str2
+#define WHEEL_STR_LIKELY(str1, str2)   (str1 == str2) [[likely]]
+#define WHEEL_STR_UNLIKELY(str1, str2) (str1 != str2) [[unlikely]]
 
 #if defined(__clang__) || defined(__GNUC__)
     #define WHEEL_ALWAYS_INLINE                             __attribute__((always_inline)) inline
     #define WHEEL_COMPTIME_NODISCARD_INLINE [[nodiscard]]   __attribute__((always_inline)) inline constexpr
-    #define WHEEL_ALWAYS_INLINE_NODISCARD  [[nodiscard]]   __attribute__((always_inline))  inline
+    #define WHEEL_ALWAYS_INLINE_NODISCARD   [[nodiscard]]   __attribute__((always_inline))  inline
 
 #elif defined(_MSC_VER)
     #define WHEEL_ALWAYS_INLINE                                          __forceinline
