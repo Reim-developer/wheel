208e806753d642ca4203e1101bea113563ca0cae
Reim-developer <contact.kaxtr@gmail.com>
2026-03-06 21:38:13 +0700

    feat(parser): add keyword enum and spec definitions

---
+++ wheel_parser/include/wheel_parser/ast/keywords.hxx
@@ -0,0 +1,29 @@
+#if !defined(KEYWORDS_HXX)
+#define KEYWORDS_HXX
+
+#include "wheel_parser/config.hxx"
+#include <cstdint>
+#include <cstddef>
+#include <array>
+#include <string_view>
+
+WHEEL_PARSER_AST_NAMESPACE
+    enum class Keyword : uint8_t {
+        Var,
+        Count
+    };
+
+    struct KeywordSpec {
+        std::string_view text;
+    };
+
+    inline constexpr std::array<KeywordSpec, static_cast<size_t>(Keyword::Count)> k_keywords = {{
+        {"var"}
+    }};
+
+
+WHEEL_PARSER_END_NAMESPACE
+
+
+
+#endif // KEYWORDS_HXX
