11fe722c3880ed81fc70a990ce8605e9226301c0
Reim-developer <contact.kaxtr@gmail.com>
2026-03-08 00:08:17 +0700

    feat(wheel_parser): add builtin registry and statement parsing to parser
    Add BuiltinRegistry and ParserStatementList to support parsing builtin
    functions and storing parsed statements. Implement parse_expression,
    parse_call_statement, and parse_statement methods with corresponding
    statement_count and statements_data accessors.

--- wheel_parser/include/wheel_parser/parser.hxx
+++ wheel_parser/include/wheel_parser/parser.hxx
@@ -12,6 +12,7 @@
 #include <wheel_memory/allocator.hxx>
 #include <wheel_memory/vec.hxx>
 #include "wheel_parser/error.hxx"
+#include "wheel_parser/ast/builtins.hxx"
 #include "wheel_parser/ast/nodes.hxx"
 #include "wheel_parser/ast/symbol.hxx"
 #include "wheel_parser/ast/keywords.hxx"
@@ -21,6 +22,7 @@ using wheel_lexer::TokenKind;
 using wheel_lexer::Lexer;
 using wheel_memory::Arena;
 using wheel_parser::ast::StatementNode;
+using wheel_parser::ast::BuiltinRegistry;
 using wheel_parser::ast::StringInterner;
 using wheel_parser::ast::Keyword;
 using std::string_view;
@@ -28,8 +30,10 @@ using std::string_view;
 WHEEL_PARSER_NAMESPACE
     #if defined(WHEEL_EXPERIMENT) && defined(WHEEL_SMALL_VEC)
         using ParserErrorList = wheel_memory::SmallVec<ParseError>;
+        using ParserStatementList = wheel_memory::SmallVec<StatementNode *>;
     #else
         using ParserErrorList = std::vector<ParseError>;
+        using ParserStatementList = std::vector<StatementNode *>;
     #endif
 
     class WheelParser {
@@ -38,21 +42,18 @@ WHEEL_PARSER_NAMESPACE
             Arena           &m_arena;
             Token           m_current_token;
             StringInterner  &m_interner;
+            BuiltinRegistry m_builtins;
             ParserErrorList m_errors;
+            ParserStatementList m_statements;
 
         private:
-            void next_token() noexcept {
-                m_current_token = m_lexer.next_token();
-            }
-
-            bool token_matches(TokenKind token_kind) const noexcept;
-            bool keyword_matches(Keyword keyword) const noexcept;
             void skip_spaces() noexcept;
             void synchronize_statement() noexcept;
             StatementNode *emit_error(ParseErrorCode code) noexcept;
-
-            const Token &consume() noexcept;
             const Token *copy_token() const noexcept;
+            wheel_parser::ast::ExpressionNode *parse_expression() noexcept;
+            StatementNode *parse_call_statement() noexcept;
+            StatementNode *parse_statement() noexcept;
 
         public:
             explicit WheelParser(Lexer &lexer, Arena &arena, StringInterner &interner) noexcept;
@@ -60,6 +61,8 @@ WHEEL_PARSER_NAMESPACE
             void parse() noexcept;
             size_t error_count() const noexcept;
             const ParseError *errors_data() const noexcept;
+            size_t statement_count() const noexcept;
+            StatementNode *const *statements_data() const noexcept;
             void clear_errors() noexcept;
             ParseDiagnosticList collect_diagnostics(std::string_view file_name = {}) const;
     };
