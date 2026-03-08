01b7ab34ebfe38809542e43c26e351ff3b668c06
Reim-developer <contact.kaxtr@gmail.com>
2026-03-08 14:16:01 +0700

    refactor(wheel_parser): restructure parser class members and remove conditional types

--- wheel_parser/include/wheel_parser/parser.hxx
+++ wheel_parser/include/wheel_parser/parser.hxx
@@ -16,6 +16,7 @@
 #include "wheel_parser/ast/nodes.hxx"
 #include "wheel_parser/ast/symbol.hxx"
 #include "wheel_parser/ast/keywords.hxx"
+#include "wheel_parser/declaration.hxx"
 
 using wheel_lexer::Token;
 using wheel_lexer::TokenKind;
@@ -28,36 +29,26 @@ using wheel_parser::ast::Keyword;
 using std::string_view;
 
 WHEEL_PARSER_NAMESPACE
-    #if defined(WHEEL_EXPERIMENT) && defined(WHEEL_SMALL_VEC)
-        using ParserErrorList = wheel_memory::SmallVec<ParseError>;
-        using ParserStatementList = wheel_memory::SmallVec<StatementNode *>;
-    #else
-        using ParserErrorList = std::vector<ParseError>;
-        using ParserStatementList = std::vector<StatementNode *>;
-    #endif
-
     class WheelParser {
-        private:
-            Lexer           &m_lexer;
-            Arena           &m_arena;
+        public:
             Token           m_current_token;
-            StringInterner  &m_interner;
-            BuiltinRegistry m_builtins;
             ParserErrorList m_errors;
+            BuiltinRegistry m_builtins;
+
+        private:
+            Lexer               &m_lexer;
+            Arena               &m_arena;
+            StringInterner      &m_interner;
             ParserStatementList m_statements;
 
         private:
-            void skip_spaces() noexcept;
             void synchronize_statement() noexcept;
-            StatementNode *emit_error(ParseErrorCode code) noexcept;
-            const Token *copy_token() const noexcept;
             wheel_parser::ast::ExpressionNode *parse_expression() noexcept;
             StatementNode *parse_call_statement() noexcept;
             StatementNode *parse_statement() noexcept;
 
         public:
             explicit WheelParser(Lexer &lexer, Arena &arena, StringInterner &interner) noexcept;
-            StatementNode *parse_variable_declaration() noexcept;
             void parse() noexcept;
             size_t error_count() const noexcept;
             const ParseError *errors_data() const noexcept;
@@ -65,6 +56,9 @@ WHEEL_PARSER_NAMESPACE
             StatementNode *const *statements_data() const noexcept;
             void clear_errors() noexcept;
             ParseDiagnosticList collect_diagnostics(std::string_view file_name = {}) const;
+
+            [[nodiscard]]
+            Token *get_current_token() noexcept;
     };
 
 WHEEL_PARSER_END_NAMESPACE
